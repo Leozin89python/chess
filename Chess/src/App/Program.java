ccbf4b6512711980c4046ed7e85f1e4c28da0202
Leozin89Python <leozin069@gmail.com>
2020-11-21 05:13:59 -0300

    chess java v1.1

--- Chess/src/App/Program.java
+++ Chess/src/App/Program.java
@@ -1,10 +1,13 @@
 package App;
 
+import BoardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-	System.out.println("Chess");
-
+	
+		Position p = new Position(8,6);
+		System.out.println(p);
+		
 	}
-
 }
