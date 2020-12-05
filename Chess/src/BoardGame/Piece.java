a72b67fd20a25179fda7044da0ce424849829dd5
Leozin89Python <leozin069@gmail.com>
2020-12-04 23:43:23 -0300

    chess v1.24

--- Chess/src/BoardGame/Piece.java
+++ Chess/src/BoardGame/Piece.java
@@ -1,6 +1,6 @@
 package BoardGame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -13,4 +13,21 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}	
+	
+	public abstract  boolean[][] PossibleMoves();
+	
+	public boolean PossibleMove(Position position) 	{
+		return PossibleMoves()[position.getRow()][position.getColumn()];	
+	}
+	
+	public boolean IsThereAnyPossibleMove() {
+		boolean[][] mat = PossibleMoves();
+		for(int i=0; i<mat.length; i++) {
+			for(int j=0; j<mat.length; j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}return false;
+	}
 }
